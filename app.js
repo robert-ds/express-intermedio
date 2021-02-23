56de34b3248679e2a94f45cd55b7146ed99fb572
Robert Vasquez <vasquez.robert.andres@gmail.com>
2021-02-22 20:23:04 -0400

    Base del Servidor

---
+++ app.js
@@ -0,0 +1,48 @@
+/* Importando Modulo Personalizado al Proyecto */
+/* var miModulo = require('./miModulo');
+ *//* Utilidades Preinstaladas de NODEJS */
+/* var util = require('util');
+
+var prueba = "10";
+console.log(util.isNumber(prueba));
+console.log(util.isString(prueba)); */
+
+
+/* Funcion básica
+function mostrarSaludo(){
+	let valor1 = "Hola";
+	let valor2 = " Mundo";
+
+	console.log(valor1 + valor2);
+}
+
+mostrarSaludo();
+
+Objecto Básico
+let curso = {
+	tema:"Nodejs",
+	leccion:"Datos compuestos"
+};
+
+console.log(curso.leccion);
+
+Objeto básico y Funciones Anonimas
+let calculo = {
+	duplicar: function(valor1){
+		return valor1 * 2;
+	},
+	divir: function(valo1){
+		return valo1/2;
+	}
+};
+
+console.log(calculo.duplicar(10), calculo.divir(10));
+
+console.log(process.platform);
+console.log(process.execPath);
+console.log(process.env);
+console.log(process.HOME); */
+
+/* miModulo.multiplicar(20); */
+
+console.log("Resultado= " + Math.floor(Math.random() * 200));
---
+++ miModulo.js
@@ -0,0 +1,12 @@
+var miModulo = {
+
+	multiplicar: function(valor1){
+		console.log(valor1 * 3);
+	},
+	dividir: function(valor1){
+		console.log(valor1 / 3);
+	}
+
+}
+
+module.exports = miModulo;
